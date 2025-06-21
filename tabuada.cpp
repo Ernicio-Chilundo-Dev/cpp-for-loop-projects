e46b88e651d3135e6ad85ae38fa0f0935ce6b97e
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-21 10:25:22 +0200

    add

--- tabuada.cpp
+++ tabuada.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
     int num;
-    cout << "Digite um numero para ver a sua tabuada: ";
+    cout << "Digite um numero para ver a tabuada: ";
     cin >> num;
 
-    for (int i = 0; i < 12; ++i)
-    {
-        cout << num<< " * " << i<< " = "<< num * i<<endl;
-
+    for(int i = 0; i <= 12; ++i){
+        cout << num << " * " << i << " = " << num * i<<endl;
     }
+
     return 0;
 }
