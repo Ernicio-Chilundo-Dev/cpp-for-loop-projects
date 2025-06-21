773bb00b78f30c1916839f0df3463abe96da5593
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-21 15:09:22 +0200

    add

---
+++ word.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string word;
+    cout << "Enter a word: ";
+    cin >> word;
+
+    for(int i = 0; i < word.length(); ++i){
+        cout<<"Letra "<<i+1<<": " <<word[i] <<endl;
+    }
+    
+    return 0;
+}
