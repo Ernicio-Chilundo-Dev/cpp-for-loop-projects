c7f3a3e8fc795609d44731c3487fef33c3b4189d
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-21 05:52:45 +0200

    solidificado o meu conhecimento com loop for

---
+++ tabuada.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Digite um numero para ver a sua tabuada: ";
+    cin >> num;
+
+    for (int i = 0; i < 12; ++i)
+    {
+        cout << num<< " * " << i<< " = "<< num * i<<endl;
+
+    }
+    return 0;
+}
